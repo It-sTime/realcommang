e20cd3550de91c9d2e586fb883b76e483f0385a9
realcommang <katlelo@naver.com>
2024-04-08 18:21:35 +0900

    [ADD] 1918 (틀린 답)

--- q1918.java
+++ q1918.java
@@ -21,29 +21,47 @@ public class q1918 {
                 sb.append(temp);
             }
             else { //연산자일때
-                if(!stack.isEmpty()) {
-                    Character top = stack.peek();
-                    if ((top == '*' || top == '/') && (temp == '+' || temp == '-')) {
-                        sb.append(temp);
-                        while (!stack.isEmpty()) {
-                            Character tmp = stack.peek();
-                            if (tmp != '(' && tmp != ')') {
-                                sb.append(tmp);
-                            }
-                            stack.pop();
-                        }
-                    }
-                    else {
-                        stack.push(temp);
-                    }
-                } else {
+                if(stack.isEmpty()) {
                     stack.push(temp);
                 }
+                else {
+                    if (temp == '(') { //(는 스택에 바로 푸시
+                        stack.push(temp);
+                    } else if (temp == ')') { // (가 나올때까지 팝
+                        while (!stack.isEmpty() && stack.peek() != '(') {
+                            Character top = stack.pop();
+                            if(top != '(') {
+                                sb.append(top);
+                            }
+                        }
+                    } else if (Rank(temp) - Rank(stack.peek()) < 0) { //스택 맨위 우선순위 > 현재 우선순위
+                        sb.append(stack.pop());
+                    } else {
+                        stack.push(temp);
+                    }
+                }
+            }
+        }
 
+        if(!stack.isEmpty()) {
+            while(!stack.isEmpty()) {
+                sb.append(stack.pop());
             }
         }
 
         System.out.println(sb);
 
+    }
+
+    static int Rank (Character a) {
+        if(a == '(' || a == ')') {
+            return 3;
+        }
+        else if(a == '*' || a == '/') {
+            return 2;
+        }
+        else {
+            return 1;
+        }
     }
 }
