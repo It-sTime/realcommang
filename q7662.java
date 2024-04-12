be2a8978215c97423b582ea1971aa4f878ccc659
realcommang <katlelo@naver.com>
2024-04-12 16:52:38 +0900

    [ADD] 7662 맞음

--- q7662.java
+++ q7662.java
@@ -1,16 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class q7662 {
+
+    static HashMap<Integer, Integer> map;
 
     public static void main(String [] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine()); //T개의 테스트 데이터
 
         for (int i = 0; i < T; i++) {
@@ -18,6 +18,7 @@ public class q7662 {
             int k = Integer.parseInt(br.readLine());
             PriorityQueue<Integer> min = new PriorityQueue<>(); // 최소힙, 큰 절반
             PriorityQueue<Integer> max = new PriorityQueue<>(Comparator.reverseOrder()); // 최대힙, 작은
+            map = new HashMap<>(); int temp = 0; int temp1 = 0;
 
             for (int j = 0; j < k; j++) {
 
@@ -28,32 +29,44 @@ public class q7662 {
                 int num = Integer.parseInt(st.nextToken());
 
                 if (Objects.equals(dOrI, "I")) {
-                    min.add(num); max.add(num);
+                    min.add(num); max.add(num); map.put(num, map.getOrDefault(num, 0)+1);
                 }
                 else if (Objects.equals(dOrI, "D")) {
-                    if(!min.isEmpty() && !max.isEmpty()) {
-                        if (num == -1) { //최솟값 삭제
-                            int temp = min.poll();
-                            max.remove(temp);
-                        } else if (num == 1) { //최댓값 삭제
-                            int temp1 = max.poll();
-                            min.remove(temp1);
-                        }
+
+                    if (num == -1) { //최솟값 삭제
+                        remove(min);
+                    } else if (num == 1) { //최댓값 삭제
+                        remove(max);
                     }
                 }
             }
-
-            if(!min.isEmpty() && !max.isEmpty()) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(max.poll());
-                sb.append(" ");
-                sb.append(min.poll());
-                sb.append('\n');
-                System.out.println(sb.toString());
+            if(!map.isEmpty()) {
+                temp = remove(max); temp1 = remove(min);
+                sb.append(temp).append(" ").append(temp1).append("\n");
             } else {
-                System.out.println("EMPTY");
+                sb.append("EMPTY\n");
             }
         }
+        System.out.println(sb.toString());
+    }
 
+    public static int remove (PriorityQueue<Integer> queue) {
+
+        while (true) {
+
+            if(!queue.isEmpty()) {
+                int result = queue.poll();
+
+                if (map.getOrDefault(result, 0) == 0) {
+                    continue;
+                } else if (map.getOrDefault(result, 0) == 1) {
+                    map.remove(result);
+                    return result;
+                } else {
+                    map.put(result, map.getOrDefault(result, 0) - 1);
+                    return result;
+                }
+            } else return 0;
+        }
     }
 }
---
+++ q7662_correct.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class q7662_correct {
+
+    static public void main (String []  args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        TreeMap<Integer, Integer> treeMap;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+
+            int k = Integer.parseInt(br.readLine());
+            treeMap = new TreeMap<>();
+
+            for (int j = 0; j < k; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+                String dOrI = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+
+                if (Objects.equals(dOrI, "D")) {
+
+                    if(treeMap.isEmpty()) continue;
+
+                    int temp;
+                    if (num == -1) { //최솟값
+                        temp = treeMap.firstKey();
+                    } else {
+                        temp = treeMap.lastKey();
+                    }
+
+                    if(treeMap.put(temp, treeMap.get(temp)-1)==1) {
+                        treeMap.remove(temp);
+                    }
+
+                } else {
+                    treeMap.put(num, treeMap.getOrDefault(num, 0)+1);
+                }
+            }
+
+
+            if (!treeMap.isEmpty()) {
+                sb.append(treeMap.lastKey()).append(" ").append(treeMap.firstKey()).append("\n");
+            } else {
+                sb.append("EMPTY\n");
+            }
+        }
+        System.out.print(sb.toString());
+    }
+
+}
